b3f12ecadbed68f7fb73683042b5c4d0217d8bf0
Matt Curtis <matt.r.curtis@gmail.com>
2012-02-01 23:38:54 +1100

    fixed for sbcl, which didn't like compiling (are)

--- t-are.lisp
+++ t-are.lisp
@@ -1,8 +1,9 @@
 (in-package #:t-sea-pie)
 
-(defmacro are (test &rest tests)
-  "Evaluate a list of tests with is"
-  `(progn
-     (is ,test)
-     (unless (null ',tests)
-       (are ,@tests))))
+(defmacro are (&rest tests)
+  "Evaluate a list of tests with is."
+  `(let ((result (gensym "result"))
+         (test (gensym)))
+     (loop :for test :in ',tests
+        do (setf result (stefil:is test)))
+     result))
